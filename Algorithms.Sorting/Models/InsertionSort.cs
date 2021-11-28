b07df14f3d64e56a11841142a50ec9204f28c07a
Stanley Novak <stanleymnovak@gmail.com>
2021-11-27 20:19:36 -0500

    Runtime xml docstring

--- Algorithms.Sorting/Models/InsertionSort.cs
+++ Algorithms.Sorting/Models/InsertionSort.cs
@@ -4,11 +4,19 @@ namespace Algorithms.Sorting.Models
 {
     public class InsertionSort : ISort
     {
+        /// <summary>
+        /// Sort in ascending order. Worst Case Runtime: O(n^2)
+        /// </summary>
+        /// <param name="arr"></param>
         public void Sort(int[] arr)
         {
             SortAsc(arr);
         }
 
+        /// <summary>
+        /// Sort in descending order. Worst Case Runtime: O(n^2)
+        /// </summary>
+        /// <param name="arr"></param>
         public void SortDesc(int[] arr)
         {
             throw new NotImplementedException();

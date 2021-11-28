df91efdf33bd05e61007b47a08b49a0f26b39c6e
Stanley Novak <stanleymnovak@gmail.com>
2021-11-27 20:42:52 -0500

    Add bubble sort

---
+++ Algorithms.Sorting/Models/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Algorithms.Sorting.Models
+{
+    public class BubbleSort : ISort
+    {
+        public void Sort(int[] arr)
+        {
+            SortAsc(arr);
+        }
+
+        public void SortDesc(int[] arr)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SortAsc(int[] arr)
+        {
+
+            for (var i = 0; i < arr.Length; i++)
+            {   
+                for (var j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        HelperMethods.Swap(arr, j, j + 1);
+                    }
+                }
+            }
+        }
+    }
+}
